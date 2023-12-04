e9c9101a8d0d82b2723fb649c9cef7d1247321f4
sba23113 <sba23113@student.cct.ie>
2023-12-04 21:22:32 +0000

    Added interface
    Scanner, text menu, exit option

--- src/coffeeshop/CoffeeShop.java
+++ src/coffeeshop/CoffeeShop.java
@@ -1,9 +1,26 @@
 package coffeeshop;
 
+import java.util.Scanner;
+
 public class CoffeeShop {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("**************");
+            System.out.println("1: show cafe menu");
+            System.out.println("2: place an order");
+            System.out.println("3: list orders");
+            System.out.println("4: fulfill order");
+            System.out.println("x: exit");
+            System.out.print("Enter request: ");
+            String action = scanner.next();
+            System.out.println("**************");
+
+            if (action.equals("x")) {
+                System.out.println("Thank you for your visit! Bye!");
+                break;
+            }            
+        }
     }
-    
 }
