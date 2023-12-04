0618407a1072da32467618bebd536102a8459a0f
sba23113 <sba23113@student.cct.ie>
2023-12-04 21:44:51 +0000

    Add show menu functionality
    Also CoffeeShop object populated with items

--- src/coffeeshop/Main.java
+++ src/coffeeshop/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        CoffeeShop cafe = new CoffeeShop();
+        cafe.addItem("Bagel", "Food", 8.50);
+        cafe.addItem("Burger", "Food", 14.50);
+        cafe.addItem("Sandwich", "Food", 11.20);
+        cafe.addItem("Quiche", "Food", 18.90);
+        cafe.addItem("Coffee", "Food", 3.50);
+        cafe.addItem("Tea", "Food", 2.50);
+        cafe.addItem("Sparkling Water", "Food", 2.00);
+        
         while (true) {
             System.out.println("**************");
             System.out.println("1: show cafe menu");
@@ -20,7 +29,12 @@ public class Main {
             if (action.equals("x")) {
                 System.out.println("Thank you for your visit! Bye!");
                 break;
-            }            
+            }                        
+            if (action.equals("1")) {
+                for (Item item : cafe.getMenu()) {
+                    System.out.println(item);
+                }
+            }
         }
     }
 }
